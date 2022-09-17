8f5e377b3b556283f22908f61bef008c1321a183
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-09-17 14:26:32 +0700

    feat: happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    transformMode: {
+      web: [/.[tj]sx$/],
+    },
+    include:[
+      "**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"
+    ]
+  },
 })
+
