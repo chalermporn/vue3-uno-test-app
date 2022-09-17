2cd1a52e7a08f7a733030930606c0c077676b0b1
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-09-17 14:59:34 +0700

    fix: auto import sort

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
 import 'uno.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 /// <reference types="vitest" />
 import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import Unocss from 'unocss/vite'
+import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Unocss()],
